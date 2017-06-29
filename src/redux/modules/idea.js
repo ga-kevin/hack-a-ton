6c093f87bed611b95c7d12374542f27414240189
Taylor E Kems <taylor.kems@generalassemb.ly>
2017-06-29 14:42:04 -0400

    defukt state

--- src/redux/modules/idea.js
+++ src/redux/modules/idea.js
@@ -2,7 +2,32 @@ export const VOTE_UP = 'redux/idea/VOTE_UP';
 export const VOTE_DOWN = 'redux/idea/VOTE_DOWN';
 
 const defaultState = {
-  ideas: []
+  ideas: [
+    {
+      id: 0,
+      title: 'title',
+      desc: 'description',
+      author: 'author',
+      type: 'build',
+      votes: 0,
+      progress: {
+        members: [],
+        status: 0
+      },
+    },
+    {
+      id: 1,
+      title: 'title2',
+      desc: 'description2',
+      author: 'author2',
+      type: 'proposal',
+      votes: 0,
+      progress: {
+        members: [],
+        status: 0
+      },
+    }
+  ]
 };
 
 // {
@@ -24,33 +49,7 @@ export default function reducer(state = defaultState, action = {}) {
   switch (action.type) {
     case VOTE_UP:
       return {
-        ...state,
-        ideas: [
-          {
-            id: 0,
-            title: 'title',
-            desc: 'description',
-            author: 'author',
-            type: 'build',
-            votes: 0,
-            progress: {
-              members: [],
-              status: 0
-            },
-          },
-          {
-            id: 1,
-            title: 'title2',
-            desc: 'description2',
-            author: 'author2',
-            type: 'proposal',
-            votes: 0,
-            progress: {
-              members: [],
-              status: 0
-            },
-          }
-        ]
+        ...state
       };
     default:
       return state;
