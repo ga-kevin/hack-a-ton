ac23193cc41f0fa6ad8b42ef837d627798c8d6cd
Taylor E Kems <taylor.kems@generalassemb.ly>
2017-06-29 13:27:29 -0400

    data contract

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to HACK A TON</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -19,3 +19,20 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
+// {
+//   data: [
+//     title,
+//     desc,
+//     author,
+//     type,
+//     vizzotes,
+//     progress: {
+//       members: [],
+//       status
+//     }
+//   ]
+// }
