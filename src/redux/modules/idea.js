71ce66b640a3ad55059bb9985bd70d236598d829
Taylor E Kems <taylor.kems@generalassemb.ly>
2017-06-29 15:45:14 -0400

    title!

--- src/redux/modules/idea.js
+++ src/redux/modules/idea.js
@@ -49,6 +49,7 @@ export function voteDown(id) {
 const water = [
   {
     id: 2,
+    title: 'interesting title!',
     desc:
       'Develop a repo structure and format that makes it easy to read and edit text content, but also readily converts to JSON for consumption by myGA.',
     author: 'matt.brendzel@generalassemb.ly',
@@ -61,6 +62,7 @@ const water = [
   },
   {
     id: 3,
+    title: 'interesting title!',
     desc:
       'In line with our "Radical Reskilling" push, it would be helpful to have a web app calculator on our site (or a microsite) where people could calculate the ROI of training by inputting current salary of a given employee (Y), desired role they want that person to move into (X), and the calculator could use industry data (cost of hiring X, average salary of X) to show how much the company would save by retraining Y instead)\n\nWe could use this calculator for lead generation (enter your email for a full report), and in sales conversations.',
     author: 'anand@generalassemb.ly',
@@ -73,6 +75,7 @@ const water = [
   },
   {
     id: 4,
+    title: 'interesting title!',
     desc: 'One search engine for Github, Confluence, JIRA, Google Docs, etc.',
     author: 'nick@generalassemb.ly',
     type: 'Build',
@@ -84,6 +87,7 @@ const water = [
   },
   {
     id: 5,
+    title: 'interesting title!',
     desc:
       'For Remote PT classes, we use Slack and Zoom. While extremely effective, they are not elegant in the way we are able to set them up. For students with one monitor in PT classes, an easy way to be able to view Zoom and Slack at the same time on the same screen while being able to toggle out Slack with an IDE, browser, Excel, etc. as needed would help with student experience. Right now students have to cascade their windows behind Slack and manually set up where Zoom and Slack go on their screens. A shell of some sort that would help the apps fit where they need to go would be an amazing way to make these two apps feel like a holistic experience.',
     author: 'adi@generalassemb.ly',
@@ -96,6 +100,7 @@ const water = [
   },
   {
     id: 6,
+    title: 'interesting title!',
     desc:
       'An Ideas board where everyone across the entire company could post ideas and they could be upvoted and discussed, allowing us to do a constant "hackathon ideas" kind of process all the time',
     author: 'timc@generalassemb.ly',
@@ -108,6 +113,7 @@ const water = [
   },
   {
     id: 7,
+    title: 'interesting title!',
     desc:
       'Set up a "Help Needed" ticket label in Jira and create a tool for discovery/exploration.',
     author: 'anna.rankin@generalassemb.ly',
@@ -120,6 +126,7 @@ const water = [
   },
   {
     id: 8,
+    title: 'interesting title!',
     desc: 'Student progress chatbot, virtual learning environment (VR), prototyping tool for accessibility, a career prep/strategy/planning development tool, mentoring platform, ',
     author: 'zach.thomas@generalassemb.ly',
     type: 'Build',
@@ -131,6 +138,7 @@ const water = [
   },
   {
     id: 9,
+    title: 'interesting title!',
     desc:
       'Instructor Dashboard that is all inclusive of training materials, student progress tracking, lectures, etc.',
     author: 'taylor.ferrari@generalassemb.ly',
@@ -143,6 +151,7 @@ const water = [
   },
   {
     id: 10,
+    title: 'interesting title!',
     desc:
       'I would love if the Outcomes Tracker could automatically pull our e-mail communications with students. We currently have to copy and paste our communications. ',
     author: 'andrew.kumar@generalassemb.ly',
@@ -155,6 +164,7 @@ const water = [
   },
   {
     id: 11,
+    title: 'interesting title!',
     desc:
       'We are using Front App to manage emails and pull customer satisfaction metrics for local Student Experience teams. Front has the ability to connect with internal systems; we would like someone to build an API that connects Front to Core so that we can have student information and communication tools available in one space. This would drastically reduce the amount of back and forth and searching local teams need to do and would streamline the process of responding to and managing student needs. ',
     author: 'samantha.barrow@generalassemb.ly',
@@ -167,6 +177,7 @@ const water = [
   },
   {
     id: 12,
+    title: 'interesting title!',
     desc:
       'Map a learning path(concepts, skills, assessment scores, etc) against Burning Glass to net out a matrix of job & salary opportunity in "real-ish" time that matches the new skills an individual has acquired from GA.',
     author: 'bryan.berger@generalassemb.ly',
@@ -179,6 +190,7 @@ const water = [
   },
   {
     id: 13,
+    title: 'interesting title!',
     desc:
       '1. Create a companion/instructor bot that accompanies you throughout the GA experience.\n\n2. Construct a dynamic GA platform that personalizes the experience the more a user interacts with it.\n\n3. Design a mobile-first GA website for tablet and/or phone that enhances the experience via device-specific functionality.\n\n4. How can GA utilize technology like augmented reality to improve the virtual classroom experience and make it more interactive?',
     author: 'kyle.eberle@generalassemb.ly',
@@ -191,6 +203,7 @@ const water = [
   },
   {
     id: 14,
+    title: 'interesting title!',
     desc:
       'Ant Design Reverse-Design/Engineering Exercise: Soon we are gonna have all these easily implementable Ant Design components at our disposal. Explore how we might employ them to improve our existing product experiences. ',
     author: 'jason@generalassemb.ly',
@@ -203,6 +216,7 @@ const water = [
   },
   {
     id: 15,
+    title: 'interesting title!',
     desc: 'Staff Learning Paths',
     author: 'jon.rojas@generalassemb.ly',
     type: 'Build',
@@ -214,6 +228,7 @@ const water = [
   },
   {
     id: 16,
+    title: 'interesting title!',
     desc: 'Code pieces',
     author: 'tim@generalassemb.ly',
     type: 'Build',
@@ -225,6 +240,7 @@ const water = [
   },
   {
     id: 17,
+    title: 'interesting title!',
     desc: 'Hackathon participation tracker',
     author: 'matthew.fritz@generalassemb.ly',
     type: 'Build',
@@ -236,6 +252,7 @@ const water = [
   },
   {
     id: 18,
+    title: 'interesting title!',
     desc: 'Help the unemployed understand how GA could help',
     author: 'chuck@ga.co',
     type: 'Proposal',
@@ -247,6 +264,7 @@ const water = [
   },
   {
     id: 19,
+    title: 'interesting title!',
     desc:
       "Automatically set Slack DND when in a meeting. Check users Google Calendar and during meetings mark them as DND. https://github.com/matiassingers/awesome-slack#javascript",
     author: 'chuck@ga.co',
@@ -259,6 +277,7 @@ const water = [
   },
   {
     id: 20,
+    title: 'interesting title!',
     desc:
       'MacOS App that allows you to mute all notifications (including Browers) with a single toggle and backed on your Calendar',
     author: 'chuck@ga.co',
@@ -271,6 +290,7 @@ const water = [
   },
   {
     id: 21,
+    title: 'interesting title!',
     desc:
       'Speech to Glossary - An Instructor is teaching a class. As they speak Students are automatically, with a minor delay be presented with the explanations and resources for specific terms that were mentioned. For example. DevOps is a field some of you might wish to look into. With platforms like AWS, Heroku and Google Cloud and tools suck as DC/OS it is an exciting time. The “Speech to Glossary” will explain what each turn in bold means and provided additional resource in real time to allow Students to remained focused on what is being said which have as much context and understanding. Would include the creation or integration into a Glossary for Students. Spike https://github.com/ChuckJHardy/SpeechToGlossary/tree/spike-one',
     author: 'chuck@ga.co',
